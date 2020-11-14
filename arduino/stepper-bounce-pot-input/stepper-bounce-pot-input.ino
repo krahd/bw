83a020657dc25ee5f0fea1a563e9c994c0550fe7
tom <tomas@laurenzo.net>
2020-11-15 00:53:14 +0800

    added arduino sample

---
+++ arduino/stepper-bounce-pot-input/stepper-bounce-pot-input.ino
@@ -0,0 +1,51 @@
+int enaPin = 5;
+int dirPin = 6;
+int stepPin = 7;
+int potPin = A0;
+
+
+void setup()
+{
+  pinMode(dirPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
+  digitalWrite(enaPin, HIGH);
+  pinMode(potPin, INPUT);
+  Serial.begin(9600);
+}
+
+
+void loop()
+
+{
+  int potValue = analogRead (potPin);
+  Serial.println (potValue); // 0 .. 1023
+
+  int steps = potValue * 2;  // 0 .. 2048
+  int delayTime = 120;
+  
+  
+
+  int i;
+  digitalWrite(dirPin, HIGH);
+  
+
+  for (i = 0; i < steps; i++) 
+  {
+    digitalWrite(stepPin, LOW);  
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(delayTime);  
+  }
+
+  digitalWrite(dirPin, LOW); // Change direction.
+  
+
+  for (i = 0; i < steps; i++) 
+  {
+    digitalWrite(stepPin, LOW);  
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(delayTime);  
+
+  }
+
+  
+}
